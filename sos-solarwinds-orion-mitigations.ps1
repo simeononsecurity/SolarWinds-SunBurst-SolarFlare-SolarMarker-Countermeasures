29db8b1cdfc77ab6dd5b837c3721678d76b957fd
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-14 16:19:43 -0600

    Update sos-solarwinds-orion-mitigations.ps1

--- sos-solarwinds-orion-mitigations.ps1
+++ sos-solarwinds-orion-mitigations.ps1
@@ -17,3 +17,13 @@ Get-Service -Name AppIdsvc | fl St*
 
 #Print Conf
 Get-AppLockerPolicy -Local
+
+#https://www.fireeye.com/blog/threat-research/2020/12/evasive-attacker-leverages-solarwinds-supply-chain-compromises-with-sunburst-backdoor.html
+New-NetFirewallRule -DisplayName "Block Outbound Solarwinds 144.86.226.0/24" -Direction Outbound –LocalPort Any -Protocol Any -Action Block -RemoteAddress 144.86.226.0/24
+New-NetFirewallRule -DisplayName "Block Outbound Solarwinds 131.228.12.0/22" -Direction Outbound –LocalPort Any -Protocol Any -Action Block -RemoteAddress 131.228.12.0/22
+New-NetFirewallRule -DisplayName "Block Outbound Solarwinds 96.31.172.0/24" -Direction Outbound –LocalPort Any -Protocol Any -Action Block -RemoteAddress 96.31.172.0/24
+New-NetFirewallRule -DisplayName "Block Outbound Solarwinds 20.140.0.0/15" -Direction Outbound –LocalPort Any -Protocol Any -Action Block -RemoteAddress 20.140.0.0/15
+New-NetFirewallRule -DisplayName "Block Inbound Solarwinds 144.86.226.0/24" -Direction Inbound –LocalPort Any -Protocol Any -Action Block -RemoteAddress 144.86.226.0/24
+New-NetFirewallRule -DisplayName "Block Inbound Solarwinds 131.228.12.0/22" -Direction Inbound –LocalPort Any -Protocol Any -Action Block -RemoteAddress 131.228.12.0/22
+New-NetFirewallRule -DisplayName "Block Inbound Solarwinds 96.31.172.0/24" -Direction Inbound –LocalPort Any -Protocol Any -Action Block -RemoteAddress 96.31.172.0/24
+New-NetFirewallRule -DisplayName "Block Inbound Solarwinds 20.140.0.0/15" -Direction Inbound –LocalPort Any -Protocol Any -Action Block -RemoteAddress 20.140.0.0/15
